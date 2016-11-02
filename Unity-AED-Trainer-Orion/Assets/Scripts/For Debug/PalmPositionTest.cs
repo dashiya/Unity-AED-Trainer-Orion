1f1f5f7fb37924fe22cad3fc18f8d460b87e9aed
dashiya <hadashiya.duck@gmail.com>
2016-11-02 16:59:50 +0900

    Implement motion of getting hand.PalmPosition
    Follow :
    https://community.leapmotion.com/t/trouble-accessing-lm-api-resources-in-unity/4774/3

--- Unity-AED-Trainer-Orion/Assets/Scripts/For Debug/PalmPositionTest.cs
+++ Unity-AED-Trainer-Orion/Assets/Scripts/For Debug/PalmPositionTest.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap;
+using Leap.Unity;
+using System.Collections.Generic;
 
 public class PalmPositionTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    LeapProvider provider;
+
+    void Start()
+    {
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+    }
+
+    void Update()
+    {
+        Frame frame = provider.CurrentFrame;
+        List<Hand> hands = frame.Hands;
+        for (int h = 0; h < hands.Count; h++)
+        {
+            Hand hand = hands[h];
+            if (hand.IsLeft | hand.IsRight )
+            {
+                transform.position = hand.PalmPosition.ToVector3() +
+                                     hand.PalmNormal.ToVector3() *
+                                    (transform.localScale.y * .5f + .02f);
+                
+            }
+        }
+    }
 }
+
