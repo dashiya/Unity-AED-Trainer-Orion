71c9d0ecb84809319861dd7a04cd9f70c6ac7df6
dashiya <hadashiya.duck@gmail.com>
2017-05-06 16:49:27 +0900

    Update flag on TempoSound

--- Unity-AED-Trainer-Orion/Assets/TempoSound.cs
+++ Unity-AED-Trainer-Orion/Assets/TempoSound.cs
@@ -5,26 +5,36 @@ public class TempoSound : MonoBehaviour
 {
 
     public AudioSource AudioSource20;
-    public double looptime;
+    
+
+    public bool isPlayforDebug;
+    private bool isLoop = false;
     // Use this for initialization
 
     void Start()
     {
-        looptime = 0.5;
+       
 
         AudioSource[] audioSources = this.GetComponents<AudioSource>();
         AudioSource20 = audioSources[0];
 
+        
         AudioSource20.playOnAwake = false;
-        AudioSource20.loop = true;
-        AudioSource20.PlayScheduled(looptime);
+   
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //for debug
-        Debug.Log(AudioSource20.isPlaying);
+        if(isPlayforDebug == false)
+        {
+            double looptime = 0.5;
+            AudioSource20.loop = true;
+            AudioSource20.PlayScheduled(looptime);
+            isPlayforDebug = true;
+        }
     }
 }
+    
