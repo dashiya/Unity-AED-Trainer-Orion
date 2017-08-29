333277a3809c3391cd7ba09337566da1ad3c8030
dashiya <hadashiya.duck@gmail.com>
2017-08-29 16:44:59 +0900

    Add TestBoxCollider

---
+++ Unity-AED-Trainer-Orion/Assets/Scripts/TestBoxCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBoxCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnColliderEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.tag == "Hand")
+        {
+            Debug.Log("手が触れたよ");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
