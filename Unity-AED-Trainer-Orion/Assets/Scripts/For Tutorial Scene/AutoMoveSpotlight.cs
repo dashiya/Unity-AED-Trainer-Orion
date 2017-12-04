3c22b21f0d9f0fac9da5dca7307f2f344c50e0e1
dashiya <hadashiya.duck@gmail.com>
2017-12-04 17:36:25 +0900

    Add code to move spotlight

--- Unity-AED-Trainer-Orion/Assets/Scripts/For Tutorial Scene/AutoMoveSpotlight.cs
+++ Unity-AED-Trainer-Orion/Assets/Scripts/For Tutorial Scene/AutoMoveSpotlight.cs
@@ -6,40 +6,41 @@ public class AutoMoveSpotlight : MonoBehaviour
 
     GameObject spotlightPrefab;
     Transform spotlightPrefabTransform;
-    float spotlightPrefabPos;
+    Vector3 spotlightPrefabPos;
 
     GameObject openButtonTutorialObject;
+    Vector3 _openButtonTutorialPos;
     Transform _openButtonTutorialTransform;//実行順 1
 
     GameObject destroyWearObject;
-    float _destroyWearPos;
+    Vector3 _destroyWearPos;
     Transform _destroyWearTransform;//実行順 2
 
-    float _pad1PositionPos;
+    Vector3 _pad1PositionPos;
     Transform _pad1PositionTransform;//実行順 3
 
-    float _pad2PositionPos;
+    Vector3 _pad2PositionPos;
     Transform _pad2PositionTransform;//実行順 5
 
-    float _buttonAudio1Pos;
+    Vector3 _buttonAudio1Pos;
     Transform _buttonAudio1Transform;//実行順 7
 
-    float _buttonAudio2Pos;
+    Vector3 _buttonAudio2Pos;
     Transform _buttonAudio2Transform;//実行順 8
 
-    float _energiationButtonPos;
+    Vector3 _energiationButtonPos;
     Transform _energiationButtonTransform;//実行順 9
 
-    float _padSetPosition1Pos;
+    Vector3 _padSetPosition1Pos;
     Transform _padSetPosition1Transform;//実行順 4
 
-    float _padSetPosition2Pos;
+    Vector3 _padSetPosition2Pos;
     Transform _padSetPosition2Transform;//実行順 6
 
-    float _CPRAudiopos;
+    Vector3 _CPRAudiopos;
     Transform _CPRAudioTransform;//実行順 10
 
-    float _tempoSoundPos;
+    Vector3 _tempoSoundPos;
     Transform _tempoSoundTransform;//実行順 11
 
     // Use this for initialization
@@ -47,32 +48,46 @@ public class AutoMoveSpotlight : MonoBehaviour
     {
         spotlightPrefab = GameObject.Find("MoveSoptlight");
         spotlightPrefabTransform = spotlightPrefab.GetComponent<Transform>();
+        spotlightPrefabPos = spotlightPrefabTransform.position;
+
 
         //OpenButton_Tutorial,DestroyWear_Tutorial取得
         //それぞれのTransformにアクセス
         //MoveSpotlightの生成
         _openButtonTutorialTransform = GameObject.Find("開閉ボタン").GetComponent<Transform>();//1
+        _openButtonTutorialPos = _openButtonTutorialTransform.position;
 
         _destroyWearTransform = GameObject.Find("Wear").GetComponent<Transform>();//2
+        _destroyWearPos = _destroyWearTransform.position;
 
         _pad1PositionTransform = GameObject.Find("パッド1").GetComponent<Transform>();//3
+        _pad1PositionPos = _pad1PositionTransform.position;
 
         _padSetPosition1Transform = GameObject.Find("PadSetPosition1").GetComponent<Transform>();//4
+        _padSetPosition1Pos = _padSetPosition1Transform.position;
 
-        _pad2PositionTransform = GameObject.Find("Pad2Position").GetComponent<Transform>();//5
+
+        _pad2PositionTransform = GameObject.Find("パッド2").GetComponent<Transform>();//5
+        _pad2PositionPos = _pad2PositionTransform.position;
+
 
         _padSetPosition2Transform = GameObject.Find("PadSetPosition2").GetComponent<Transform>();//6
+        _padSetPosition2Pos = _padSetPosition2Transform.position;
 
         _buttonAudio1Transform = GameObject.Find("ButtonAudio1").GetComponent<Transform>();//7
+        _buttonAudio1Pos = _buttonAudio1Transform.position;
 
         _buttonAudio2Transform = GameObject.Find("ButtonAudio2").GetComponent<Transform>();//8
+        _buttonAudio2Pos = _buttonAudio2Transform.position;
 
-         _energiationButtonTransform  = GameObject.Find("ボタン内側").GetComponent<Transform>();//9
+        _energiationButtonTransform = GameObject.Find("ボタン内側").GetComponent<Transform>();//9
+        _energiationButtonPos = _energiationButtonTransform.position;
 
         _CPRAudioTransform = GameObject.Find("CPRAudio").GetComponent<Transform>();//10
+        _CPRAudiopos = _CPRAudioTransform.position;
 
         _tempoSoundTransform = GameObject.Find("TempoSound").GetComponent<Transform>();//11
-
+        _tempoSoundPos = _tempoSoundTransform.position;
 
 
 
@@ -83,6 +98,27 @@ public class AutoMoveSpotlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         //OpenButton_Tutorial,DestroyWear_Tutorial内、flag[0]と、isPlayAudioDestroyWearを監視、それぞれがtrueになったタイミングで上で取得したtransformつかってprefab化したspotlightを移動してチュートリアルを進める
+
+       // spotlightPrefabPos = new Vector3(-1.296f, 4.133f, 0.81f);
+        spotlightPrefabPos.x = -1.296f;
+        spotlightPrefabPos.z = 0.81f;
+
+        transform.position = spotlightPrefabPos;
+
+           Debug.Log(_destroyWearPos + "_destroyWearPos");
+
+        /*
+                _pad1Pos = transform.position;
+
+            _pad1Pos.x = _handPos.ConvertPosition.x;
+            _pad1Pos.y = _handPos.ConvertPosition.y;
+            _pad1Pos.z = _handPos.ConvertPosition.z;
+
+            transform.position = _pad1Pos;
+            */
+
     }
 }
